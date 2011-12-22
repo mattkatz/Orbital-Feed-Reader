8f21c724049c16afc3144a51c483204729565769
Matt Katz <gitorious@morelightmorelight.com>
2011-12-21 22:20:13 -0500

    trying to get an activation_hook to work

--- plugin/wordprss.php
+++ plugin/wordprss.php
@@ -15,14 +15,32 @@ $capability = 'edit_posts';
 $slug = 'wordprss.php';
 global $wordprss_db_version;
 $wordprss_db_version = '0.1';
+global $wordprss_db_version_opt_string;
+$wordprss_db_version_opt_string = 'wordprss_db_version';
 
+register_activation_hook(__FILE__,'wprss_install_db');
+
+
+function wprss_plugin_menu(){
+  $hook = add_menu_page('WordPrss', 'Consume','edit_posts','wordprss.php','generate_main_page');
+
+}
+function generate_main_page()
+{
+  echo '<p>IT WORKS</p>' . '<p> wordprss version ' . get_option('wordprss_db_version',"NOTHING"). '</p>';
+  echo '<p>IT WORKS</p>' . '<p> wordprss version ' . get_option('admin_email',"NOTHING"). '</p>';
+}
 
 # create the database tables.
 # TODO extract this to a sep file
 function wprss_install_db()
 {
-  global $wpdb;
-  global $wordprss_db_version;
+  //global $wpdb;
+  //global $wordprss_db_version;
+  //add_option($wordprss_db_version_opt_string,$wordprss_db_version);
+  add_option('wordprss_db_version','0.1');
+  /*
+
   $table_name = $wpdb->prefix.$tbl_prefix."feeds";
 
   $sql = "CREATE TABLE " . $table_name ." (
@@ -33,20 +51,10 @@ function wprss_install_db()
     site_url varchar(250) not null default '',
     UNIQUE KEY id (id)
   );";
+   */
 
-  require_once(ABSPATH. 'wp_admin/includes/upgrade.php');
-  dbDelta($sql);
+  //require_once(ABSPATH. 'wp_admin/includes/upgrade.php');
+  //dbDelta($sql);
 }
-
-
-function wprss_plugin_menu(){
-  $hook = add_menu_page('WordPrss', 'Consume','edit_posts','wordprss.php','generate_main_page');
-
-}
-function generate_main_page()
-{
-  echo '<p>IT WORKS</p>';
-}
-
 add_action('admin_menu', 'wprss_plugin_menu');
 ?>
