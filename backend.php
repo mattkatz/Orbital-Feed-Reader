bc8eb9a6b5f4dbbb8cf8babde0d21ee74104d7bb
Matt Katz <gitorious@morelightmorelight.com>
2012-01-01 09:47:21 -0500

    extract nonce dance to sep method

--- backend.php
+++ backend.php
@@ -1,19 +1,24 @@
 <?php
 
+function nonce_dance(){
+  $nonce = $_GET['nonce_a_donce'];
+
+  // check to see if the submitted nonce matches with 
+  // the generated nonce we created earlier
+  if ( ! wp_verify_nonce( $nonce, 'nonce_a_donce' ) ){
+      die ( 'Busted!');
+  }
+
+}  
+
 function wprss_list_feeds(){
 
   global $wpdb;
   global $tbl_prefix;
   //echo $tbl_prefix . " WAHEY";
-  $nonce = $_GET['nonce_a_donce'];
+  nonce_dance();
   //TODO check to see what current user is 
-
-  // check to see if the submitted nonce matches with the
-  // generated nonce we created earlier
-  if ( ! wp_verify_nonce( $nonce, 'nonce_a_donce' ) ){
-      die ( 'Busted!');
-  }
-  
+  //TODO qualify this to just a user  
   $table_name = $wpdb->prefix.$tbl_prefix. "feeds";
   $sql = "select * from ".$table_name ;
   $myrows = $wpdb->get_results($sql );
@@ -22,4 +27,8 @@ function wprss_list_feeds(){
 }
 add_action('wp_ajax_wprss_get_feeds','wprss_list_feeds');
 add_action('wp_ajax_nopriv_wprss_get_feeds','wprss_list_feeds');
+
+function wprss_update_feeds(){
+
+}
 ?>
