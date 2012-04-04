9c11abddda4c23a1ad95c56081c035f769dff894
Matt Katz <gitorious@morelightmorelight.com>
2012-04-04 19:46:53 -0400

    adding in the feed mgmt screen

---
+++ feed_management.php
@@ -0,0 +1,13 @@
+<h1>Here there should be a list of your feeds</h1>
+<p>
+ You should be able to:
+  <ul>
+    <li>Remove feeds</li>
+    <li>Add feeds</li>
+    <li>Change the privacy setting of a feed</li>
+    <li>Add feeds to tags</li>
+  </ul>
+</p>
+<?php
+
+?>
