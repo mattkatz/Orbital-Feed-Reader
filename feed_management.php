2d1ce38c0e03459b2fbd9cfcc2871f2eec5e6881
Matt Katz <gitorious@morelightmorelight.com>
2012-04-04 20:26:22 -0400

    add some dummy content to the subscribe window and style it a bit.

--- feed_management.php
+++ feed_management.php
@@ -1,4 +1,31 @@
 <h1>Here there should be a list of your feeds</h1>
+<div id="feedListContainer">
+  <ul name="wprss-feedlist" id="feedlist" class="wprss-feedlist" >
+    <li class="item" >Blah</li>
+    <li> Lah-de-dah </li>
+    <li class="item">
+      BungBungBung
+    </li>
+    <li>
+      Flooogs!
+    </li> 
+    <li class="item is-selected">
+      ChurningNews
+    </li> 
+    <li class="item">
+      RimbleBleg
+    </li>
+  </ul>
+</div>  
+<div id="detailContainer">
+  <input type="text" name="Feed Name" value="ChurningNews" id="feedName"/>
+  <label for="feedUrl">Feed Url</label>
+  <input type="text" name="feedUrl" value="http://churning.org/feed" id="feedUrl"/> 
+  <label for="siteUrl">Site Url</label>
+  <input type="text" name="siteUrl" value="http://churning.com" id="siteUrl"/>
+
+</div>
+
 <p>
  You should be able to:
   <ul>
