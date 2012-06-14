893a877e566abbdb0bed23bf68a18e8956b1e973
Matt Katz <gitorious@morelightmorelight.com>
2012-06-14 12:28:41 -0400

    export done.

--- export_opml.php
+++ export_opml.php
@@ -2,7 +2,31 @@
 // Set the headers so the file downloads
 header('Content-type: application/xml+opml');
 header('Content-Disposition: attachment; filename="Wordprss-OPML-Export.xml"');
-
-echo "a,b,c";
+//header('Content-type: text/xml');
+//oh PHP.  Why you gotta be so difficult about escaping things?
+echo "<?xml version=\"1.0\" encoding=\"utf-8\"?".">";
+?>
+<opml version="1.0" >
+  <head>
+    <dateCreated>
+      <?php echo date("r", time())?>
+    </dateCreated>
+    <title>
+      WordpRSS OPML Feed Export
+    </title>
+  </head>
+  <body>
+    <?php
+    require_once 'backend.php';
+    $feeds = WprssFeeds::get();
+    foreach($feeds as $feed){
+    ?>
+      <outline text="<?php echo $feed->feed_name?>" title="<?php echo $feed->feed_name?>" type="rss" xmlUrl="<?php echo $feed->feed_url?>" htmlUrl="<?php echo $feed->site_url?>"/>
+    <?php
+    }
+    ?>
+  </body>
+</opml>
+<?php
 exit;
 ?>
