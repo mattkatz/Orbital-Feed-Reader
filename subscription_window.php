8041e121dbced7d9688ee2346d9ec631dc387326
Matt Katz <gitorious@morelightmorelight.com>
2012-07-14 11:06:49 -0400

    formatting

--- subscription_window.php
+++ subscription_window.php
@@ -1,55 +1,53 @@
+<div id="subscribe-window" class="modal-window invisible">
+<script type="text/x-handlebars">
+  {{#view Wprss.FeedsForm}}
+    {{view Em.TextField 
+      placeholder="Drag or copy paste a feed here" 
+      viewName="urlField"}}
+    <button type='submit'>Add Feed</button>
+  {{/view}}
 
-  <div id="subscribe-window" class="modal-window invisible">
-  <script type="text/x-handlebars">
-    {{#view Wprss.FeedsForm}}
-      {{view Em.TextField 
-        placeholder="Drag or copy paste a feed here" 
-        viewName="urlField"}}
-      <button type='submit'>Add Feed</button>
-    {{/view}}
-
-      <div class="horizontal-form">
-        
-        {{#if Wprss.feedFinder.feedCandidate}}
-          {{#view Wprss.FeedView }}
-          {{#with Wprss.feedFinder.feedCandidate}}
-              {{view Em.TextField valueBinding="feed_name" class="heading" }}
-              <label>Feed Url
-              {{view Em.TextField valueBinding="feed_url" }}
-              </label>
-              <label>Site Url
-                {{view Em.TextField valueBinding="site_url" }}
-              </label>
+    <div class="horizontal-form">
+      
+      {{#if Wprss.feedFinder.feedCandidate}}
+        {{#view Wprss.FeedView }}
+        {{#with Wprss.feedFinder.feedCandidate}}
+            {{view Em.TextField valueBinding="feed_name" class="heading" }}
+            <label>Feed Url
+            {{view Em.TextField valueBinding="feed_url" }}
+            </label>
+            <label>Site Url
+              {{view Em.TextField valueBinding="site_url" }}
+            </label>
+            <label>
+              {{view Em.Checkbox valueBinding="is_private" title="This Feed is Private! Don't show it to other people."}}
+            </label>
+            {{#if  feed_id}}
               <label>
-                {{view Em.Checkbox valueBinding="is_private" title="This Feed is Private! Don't show it to other people."}}
+                Get rid of this feed! Seriously! 
+                {{#view Em.Button target="Wprss.selectedFeedController" action="unsubscribe"}} Unsubscribe {{/view}}
               </label>
-              {{#if  feed_id}}
-                <label>
-                  Get rid of this feed! Seriously! 
-                  {{#view Em.Button target="Wprss.selectedFeedController" action="unsubscribe"}} Unsubscribe {{/view}}
-                </label>
-              {{/if}}
-              <div>
-              {{#view Em.Button target="Wprss.feedFinder" action="saveFeed" }}Save{{/view}}
+            {{/if}}
+            <div>
+            {{#view Em.Button target="Wprss.feedFinder" action="saveFeed" }}Save{{/view}}
+            </div>
+        {{/with }}
+        {{/view}}
+      {{/if}}
+      {{#if Wprss.feedFinder.possibleFeeds }}
+        <div>
+          We found {{Wprss.feedFinder.possibleFeeds.length }} feeds:
+        </div>
+        {{#each Wprss.feedFinder.possibleFeeds}}
+          {{#view Wprss.PossibleFeedView contentBinding="this"}}
+            {{#with content}}
+              <div class="possibleFeed">
+              {{url}}
               </div>
-          {{/with }}
+            {{/with}}
           {{/view}}
-        {{/if}}
-        {{#if Wprss.feedFinder.possibleFeeds }}
-          <div>
-            We found {{Wprss.feedFinder.possibleFeeds.length }} feeds:
-          </div>
-          {{#each Wprss.feedFinder.possibleFeeds}}
-            {{#view Wprss.PossibleFeedView contentBinding="this"}}
-              {{#with content}}
-                <div class="possibleFeed">
-                {{url}}
-                </div>
-              {{/with}}
-            {{/view}}
-          {{/each}}
-        {{/if}}
-      </div>
-  </script>
-
-  </div>
+        {{/each}}
+      {{/if}}
+    </div>
+</script>
+</div>
