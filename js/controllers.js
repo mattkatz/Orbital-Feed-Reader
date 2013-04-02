199673d4d1a4ab4c8cff8c5324a01429fdf92575
matt <matt@kingofrats.(none)>
2013-04-01 22:48:06 -0400

    Comment harder

--- js/controllers.js
+++ js/controllers.js
@@ -3,11 +3,18 @@
 function FeedListCtrl($scope, $http, $log){
   $log.log('in feedscontrol');
   $scope.log = $log.log;
+
+  /*
+   * let the world know a feed has been CHOSEN
+   * Mightily emit a roar up the scope chain
+   */
   $scope.select = function(id){
     $scope.log(id);
     $scope.$emit('feedSelect', {feed_id: id});
-    //EntriesCtrl.refresh(id);
   };
+  /*
+   * get the list of feeds and store it
+   */
   $scope.refresh = function (){
     $http.get(get_url.ajaxurl+'?action=wprss_get_feeds' )
     .success(function(data){ 
@@ -20,22 +27,23 @@ function FeedListCtrl($scope, $http, $log){
 function EntriesCtrl($scope, $http, $log){
   $scope.log = $log.log;
   $scope.log("in EntriesCtrl");
-  $scope.refresh = function(id){
+  /*
+   * select a feed to display entries from
+   */
+  $scope.select = function(id){
     $scope.log('Getting feed '+id);
     $http.get(get_url.ajaxurl+'?action=wprss_get_entries&feed_id='+id)
     .success(function(data){
       $scope.entries = data;
     });
   };
-  $scope.refresh();
+  $scope.select();
+  /*
+   * Catch the feedSelected event, display entries from that feed
+   */
   $scope.$on('feedSelected',function(event,args){
     $scope.log('feedSelected in Entries!');
     $scope.refresh(args.feed_id);
   });
 }
-
-function TestCtrl($scope, $http,$routeParams,$log) {
-  $log.log('in test ctrl');
-}
-
 
