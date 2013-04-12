4503e1134540aeda975ac2865918b1f17618c0d0
matt <matt@kingofrats.(none)>
2013-04-12 17:55:41 -0400

    Adding attribution (date, author) to entries
    The date we get from mysql seems to be formatted
    
    YYYY-MM-DD HH:mm:ss
    
    and the angular date filter wants
    
    YYYY-MM-DDTHH:mm:ss
    
    so I'll have to fix that on the backend I guess.

--- mainwindow.php
+++ mainwindow.php
@@ -9,6 +9,12 @@
           <li class="entry" ng-repeat="entry in entries" >
               <div id="{{entry.feed_id}}_{{entry.entry_id}}"ng-class="{'is-read': entry.isRead == 1, 'is-current': entry.entry_id == selectedEntry.entry_id}" >
                 <a href="{{entry.link}}"><h2 class="entry-title" ng-bind-html="entry.title"></h2></a>
+                <div class="author" ng-show="entry.author">
+                  {{entry.author}}
+                </div>
+                <div class="date">
+                  {{entry.entered | date:medium }}
+                </div>
                 <div ng-click="selectEntry(entry)" class="entry-content" ng-bind-html="entry.content"></div>
               </div>
           </li>
