fa26839c069506d07ee5c7f6422c768f8e8e00e7
Matt Katz <mk@new-host-3.home>
2013-08-12 18:44:40 -0400

    fix entry id mismatch between display and scrollto
    entry_id is non-unique, points to the overall entry
    not the users entry.

--- mainwindow.php
+++ mainwindow.php
@@ -15,7 +15,7 @@
           ?>">
         </div>
         <ul id='orbital-entries' class="entries" infinite-scroll="addMoreEntries()" infinite-scroll-disabled='isLoading' infinite-scroll-parent='true' infinite-scroll-distance="2" >
-          <li id="{{entry.feed_id}}_{{entry.entry_id}}" class="entry" ng-repeat="entry in entries" ng-class="{'is-read': entry.isRead == 1, 'is-current': entry.entry_id == selectedEntry.entry_id}" >
+          <li id="{{entry.feed_id}}_{{entry.id}}" class="entry" ng-repeat="entry in entries" ng-class="{'is-read': entry.isRead == 1, 'is-current': entry.id == selectedEntry.id}" >
               <a href="{{entry.link}}"><h2 class="entry-title" ng-bind-html="entry.title"></h2></a>
               <div class="author" ng-show="entry.author">
                 {{entry.author}}
