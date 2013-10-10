b2c1025aa44b2c4a07c27e3b6660833300e5da02
Matt Katz <mk@The-Silver-Wedge.local>
2013-10-09 20:29:58 -0400

    Let's remove some logging bc

--- js/controllers.js
+++ js/controllers.js
@@ -603,9 +603,9 @@ function CommandBarCtrl($scope,$http,$log,feedService){
     $scope.$emit('commandBarEvent',{name: action.name,feed: $scope.currentFeed});
   };
   $scope.changeSortOrder = function(){
-    console.log("pre saving " + $scope.sortOrder);
+    //console.log("pre saving " + $scope.sortOrder);
     feedService.saveSort($scope.sortOrder);
-    console.log("post saving " + $scope.sortOrder);
+    //console.log("post saving " + $scope.sortOrder);
   };
   $scope.commands = [
     { title: "Mark All As Read",
