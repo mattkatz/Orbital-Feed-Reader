1ccbc29e41788dee2f58e7b797f71d095c8a60f7
Matt Katz <git@morelightmorelight.com>
2014-07-02 20:41:48 -0400

    upgrade to latest version of autocomplete directive
    works on chrome since chrome doesn't seem to have contains in js.

--- js/autocomplete-directive.js
+++ js/autocomplete-directive.js
@@ -144,3 +144,9 @@ angular.module('autocomplete-directive',[])
     },
   }
 });
+//Polyfill contains for old browsers
+if (!('contains' in String.prototype)) {
+    String.prototype.contains = function(str, startIndex) {
+          return -1 !== String.prototype.indexOf.call(this, str, startIndex);
+            };
+}
