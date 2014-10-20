b69427524dedc861efd4aa88fae0593d9d59e928
Matt Katz <git@morelightmorelight.com>
2014-10-19 21:40:51 -0400

    icons are better than words sometimes

--- mainwindow.php
+++ mainwindow.php
@@ -34,7 +34,7 @@
     </ul>
   </div>
   <div id='orbital-cli' class="modal-window" ng-show="reveal" ng-controller="CliCtrl">
-    <div>Start typing the name of a feed <span title='soon, soon..'><strike>or tag</strike></span>. Arrow keys or tab select a feed from the list. Enter goes to whatever you've selected. Esc closes the window.</div>
+    <div>Start typing the name of a feed <span title='soon, soon..'><strike>or tag</strike></span>. ⬇ ⬆  select a feed from the list. Enter goes to whatever you've selected. Esc closes the window.</div>
     <input id='orbital-cli-input' ng-model='filterstring' focus-me='reveal' ng-keyup='processKeys($event)' type='text'></input>
     <div id='orbital-cli-results' ng-show='filterstring'>
       <ul class='feeds'>
