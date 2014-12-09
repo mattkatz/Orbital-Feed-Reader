7ba303e39c620672885d7b818d7e40ac83f59cf7
Matt Katz <git@morelightmorelight.com>
2014-12-09 08:38:32 -0500

    Add OPML icon to settings

--- settings.php
+++ settings.php
@@ -2,7 +2,7 @@
     <div class="wrap">
         <h2>Orbital Settings</h2>
     <p>If you are looking for a way to edit the names or members of your feeds, click the pencil icon (✎) on the feed list.</p>
-    <p>You can <a href="<?php echo site_url("?export_opml=".get_current_user_id())  ?>">export your feed list in OPML format</a> at any time. If you are logged in, you'll get your <b>private</b> feeds in there. If you aren't logged in, the same link will filter out private feeds. This is useful if you want to move your list to another feed reader. </p>
+    <p>You can <a href="<?php echo site_url("?export_opml=".get_current_user_id())  ?>">export your feed list in OPML format<img id="orbital-opml-icon" class="orbital-opml-icon" width=10 height=10 src="<?php echo plugins_url("img/opml-icon.svg", __FILE__); ?>"/></a> at any time. If you are logged in, you'll get your <b>private</b> feeds in there. If you aren't logged in, the same link will filter out private feeds. This is useful if you want to move your list to another feed reader. </p>
     <p><small>If they don't give you this same option, maybe think twice about using that kind of software.</small></p>
       <form action="options.php" method="POST">
         <?php settings_fields( 'orbital-settings-group' ); ?>
