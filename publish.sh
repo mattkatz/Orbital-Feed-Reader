61e43600c46b5b8a8c868cf876de402b3c96ea63
Matt Katz <git@morelightmorelight.com>
2015-01-07 20:19:58 -0500

    lets make sure publish.sh works

--- publish.sh
+++ publish.sh
@@ -3,12 +3,12 @@ curbranch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 pushd .ditz
 ditz html
 popd
-git add .ditz
-git commit .ditz -m "auto committing ditz html files"
+#git add .ditz
+#git commit .ditz -m "auto committing ditz html files"
 git checkout gh-pages
-git checkout $curbranch .ditz/html
+#git checkout $curbranch .ditz/html
 cp -R .ditz/* ditz
-git add .ditz
+#git add .ditz
 git add ditz
 git commit -m "auto committing ditz html files"
 git push
